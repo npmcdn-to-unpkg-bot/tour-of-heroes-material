3b0ffac73eff90059eda7791c962fcee47c80e0c
Tim Dekarz <Tim Dekarz>
2016-06-08 22:14:21 +0200

    Initial commit

---
+++ app/heroes.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router-deprecated';
+
+import { Hero }                from './hero';
+import { HeroService }         from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: '../views/heroes.component.html',
+    //styleUrls:  ['../styles/heroes.component.css'],
+    directives: [HeroDetailComponent]
+})
+export class HeroesComponent implements OnInit {
+    heroes: Hero[];
+    selectedHero: Hero;
+    addingHero = false;
+    error: any;
+
+    constructor(
+        private router: Router,
+        private heroService: HeroService) { }
+
+    getHeroes() {
+        setTimeout(() => this.heroService
+            .getHeroes()
+            .then(heroes => this.heroes = heroes)
+            .catch(error => this.error = error), 2000); // TODO: Display error message
+    }
+
+    addHero() {
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+        this.addingHero = false;
+        if (savedHero) { this.getHeroes(); }
+    }
+
+    delete(hero: Hero, event: any) {
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(res => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            })
+            .catch(error => this.error = error); // TODO: Display error message
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+        this.addingHero = false;
+    }
+
+    gotoDetail() {
+        this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    }
+}
---
+++ systemjs.config.js
@@ -0,0 +1,55 @@
+(function(global) {
+
+    var ngVer = '@2.0.0-rc.1'; // lock in the angular package version; do not let it float to current!
+
+    //map tells the System loader where to look for things
+    var map = {
+        'app':                        'dist', //dev='app' || prod='dist'
+        '@angular':                   'node_modules/@angular',
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        'rxjs':                       'node_modules/rxjs'
+    };
+
+    //packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        'app':                        { main: 'main.js',  defaultExtension: 'js' },
+        'rxjs':                       { defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    };
+
+    var ngPackageNames = [
+        'common',
+        'compiler',
+        'core',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router',
+        'router-deprecated',
+        'upgrade',
+    ];
+
+    // Add map entries for each angular package
+    // only because we're pinning the version with `ngVer`.
+    ngPackageNames.forEach(function(pkgName) {
+        map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
+    });
+
+    // Add package entries for angular packages
+    ngPackageNames.forEach(function(pkgName) {
+
+        // Bundled (~40 requests):
+        packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+
+        // Individual files (~300 requests):
+        //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    });
+
+    var config = {
+        map: map,
+        packages: packages
+    };
+
+    System.config(config);
+
+})(this);
